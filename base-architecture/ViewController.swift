b9ce807ce58404b67e28460cc983137577ab6c45
R-KazumaInazaki <kazuma.inazaki+rakuma@connec10.co.jp>
2022-02-17 14:57:08 +0900

    Initial Commit

---
+++ base-architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  base-architecture
+//
+//  Created by Kazuma Inazaki on 2022/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
